a7496cd55cea4bb967d8aa142f7ed1b414d6ebf3
Applad <Dennisvonkannen@gmx.de>
2014-07-19 17:51:55 +0200

    Update

--- hl2rp/plugins/combineweps/items/weapon/sh_wep_smg1_bl.lua
+++ hl2rp/plugins/combineweps/items/weapon/sh_wep_smg1_bl.lua
@@ -2,7 +2,7 @@ ITEM.name = "Standard Issue MP7"
 ITEM.uniqueID = "weapon_smg1"
 ITEM.category = "City 45 | Weaponry"
 ITEM.model = Model("models/weapons/w_smg1.mdl")
-ITEM.class = "weapon_smg1"
+ITEM.class = "weapon_smg1_bl"
 ITEM.type = "primary"
 ITEM.price = 0
 ITEM.desc = "A metallic submachinegun with a dark metallic undertone and a holographic sight.\nThere are %ClipOne|0% shots left in the magazine.\n<:: triggerQuery: REQUIRE SIGNAL = %CombineLocked|0% ::> "
